883d2ca70a298dd1d612a595320c4ad78cf72a7a
enavagoro <enavarrete@samurai.cl>
2021-03-09 18:24:48 -0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  ip = '192.168.1.1';
+  store = 'Mon Amour';
+  array = [1,2,3,4]
+  value = true;
 
   constructor() {}
 
+  changeValue(){
+    this.value = !this.value;
+  }
 }
